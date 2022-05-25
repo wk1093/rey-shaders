eea2971752c40ab1e8d3a5f3127399167baea81e
wk1093 <69118843+wk1093@users.noreply.github.com>
2022-05-25 13:32:17 -0700

    Create leaf.glsl

---
+++ leaf.glsl
@@ -0,0 +1,50 @@
+precision highp float;
+attribute vec3 position;
+attribute vec3 normal;
+uniform mat3 normalMatrix;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+varying vec3 fNormal;
+varying vec3 fPosition;
+uniform float time;
+
+
+float random (in vec2 _st) {
+    return fract(sin(dot(_st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+// Based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise (in vec2 _st) {
+    vec2 i = floor(_st);
+    vec2 f = fract(_st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+
+void main()
+{
+  fNormal = normalize(normalMatrix * normal);
+  vec4 pos = modelViewMatrix * vec4(position, 1.0);
+  fPosition = pos.xyz;
+  gl_Position = projectionMatrix * pos;
+  gl_Position = vec4(
+    gl_Position.x+.2*noise(gl_Position.xy*sin(time*18.4567)),
+    gl_Position.y+.2*noise(gl_Position.xz*sin(time*8.234)),
+    gl_Position.z+.2*noise(gl_Position.zy*sin(time*7.3452)),
+    gl_Position.a+.2*noise(gl_Position.zx*sin(time*9.345))
+  );
+}
